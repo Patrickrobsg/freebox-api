a2bf2bb2a97f6ab8e5175077aef508eaddd76e76
Patrickrobsg <patrickrobsg@hotmail.com>
2023-09-23 16:16:59 -0300

    Commit Inicial

---
+++ src/main/java/com/freebox/freeboxapi/FreeboxApiApplication.java
@@ -0,0 +1,13 @@
+package com.freebox.freeboxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeboxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeboxApiApplication.class, args);
+	}
+
+}
